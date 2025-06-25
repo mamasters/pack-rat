f2d932c1ba558390cc9928d43fa0ce0175a0eb98
Michael Masters <mastersm89@gmail.com>
2025-06-24 19:58:03 -0600

    initial commit  - Book Class  - Book Dao

---
+++ app/src/main/java/com/example/packrat/data/local/Book.kt
@@ -0,0 +1,17 @@
+package com.example.packrat.data.local
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "books",
+    indices = [Index(value = ["title"]), Index(value = ["isbn"], unique = true)],
+)
+data class Book (
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val author: String,
+    val isbn: String?,
+    val unitPrice: Double?,
+)
